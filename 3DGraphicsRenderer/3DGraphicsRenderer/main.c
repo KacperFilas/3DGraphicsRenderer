870706c97df99cb600b1f63bcf9b0baae01ef34e
Kacper Filas <kacper.filas1@gmail.com>
2026-03-07 18:11:27 +0100

    Allocated bytes for collor buffer and cleaned the memory on project destroy

--- 3DGraphicsRenderer/3DGraphicsRenderer/main.c
+++ 3DGraphicsRenderer/3DGraphicsRenderer/main.c
@@ -1,10 +1,14 @@
 #include <stdio.h>
 #include <stdbool.h>
+#include <stdint.h>
 #include <SDL.h>
 
 bool is_running = false;
 SDL_Window* window = NULL;
 SDL_Renderer* renderer = NULL;
+uint32_t* color_buffer = NULL;
+int window_width = 800;
+int window_height = 600;
 
 bool initialize_window(void){
     // Initialize SDL
@@ -18,8 +22,8 @@ bool initialize_window(void){
         NULL,
         SDL_WINDOWPOS_CENTERED,
         SDL_WINDOWPOS_CENTERED,
-        800,
-        600,
+        window_width,
+        window_height,
         SDL_WINDOW_BORDERLESS
      );
 
@@ -41,7 +45,8 @@ bool initialize_window(void){
 }
 
 void setup(void){
-
+    // Initialize the color buffer
+    color_buffer = (uint32_t*) malloc(sizeof(uint32_t) * window_width * window_height);
 }
 
 void process_input(void){
@@ -61,15 +66,21 @@ void process_input(void){
 }
 
 void update(void){
-
+    //todo
 }
-
 
 void render(void){
     SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
     SDL_RenderClear(renderer);
     SDL_RenderPresent(renderer);
     
+}
+
+void destroy_window(void){
+    free(color_buffer);
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
 }
 
 
@@ -85,5 +96,7 @@ int main(void){
         render();
     }
 
+    destroy_window();
+
     return 0;
-}
+}  
