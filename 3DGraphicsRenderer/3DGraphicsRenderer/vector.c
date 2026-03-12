30e23b238f4467b4884fcfb1fec82fb4dcf1e440
Kacper Filas <kacper.filas1@gmail.com>
2026-03-12 18:33:55 +0100

    Added rotation to the cube

--- 3DGraphicsRenderer/3DGraphicsRenderer/vector.c
+++ 3DGraphicsRenderer/3DGraphicsRenderer/vector.c
@@ -1,3 +1,48 @@
+#include <math.h>
 #include "vector.h"
 
-// Implemanation of vector functions.
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Properties of the cos and sin functions
+// x = r * cos(beta)
+// y = r * sin(beta)
+
+// Final rotation matrix equeations derived from the properties of the cos and sin functions
+// x' = r * cos(alfa + beta)
+// x' = r * (cos(alfa) * cos(beta) - sin(alfa) * sin(beta)) -- angle addition law
+// x' = x * cos(beta) - y * sin(beta)
+
+// y' = r * sin(alfa + beta)
+// y' = r * (sin(alfa) * cos(beta) + cos(alfa) * sin(beta)) -- angle addition law
+// y' = x * sin(beta) + y * cos(beta)
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+vec3_t vec3_rotate_x(vec3_t v, float angle){
+    vec3_t rotated_vector = {
+        .x = v.x,
+        .y = v.y * cos(angle) - v.z * sin(angle),
+        .z = v.y * sin(angle) + v.z * cos(angle)
+    };
+
+    return rotated_vector;
+}
+
+vec3_t vec3_rotate_y(vec3_t v, float angle){
+    vec3_t rotated_vector = {
+        .x = v.x * cos(angle) + v.z * sin(angle),
+        .y = v.y,
+        .z = v.x * sin(angle) - v.z * cos(angle)
+    };
+
+    return rotated_vector;
+}
+
+vec3_t vec3_rotate_z(vec3_t v, float angle){
+    vec3_t rotated_vector = {
+        .x = v.x * cos(angle) - v.y * sin(angle),
+        .y = v.x * sin(angle) + v.y * cos(angle),
+        .z = v.z
+    };
+
+    return rotated_vector;
+}
